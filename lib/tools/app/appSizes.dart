0ed0cbbf106bb192e27607cf4fcb5a535239ad0f
ali bagheri <bagheri_a@nicode.org>
2023-02-07 18:29:48 +0330

    1401-11-18, c2

--- lib/tools/app/appSizes.dart
+++ lib/tools/app/appSizes.dart
@@ -3,8 +3,8 @@ import 'dart:ui' as ui;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-typedef OnResizeScreen = Function(double oldW, double oldH, double newW, double newH);
-///--------------------------------------------------------------------------------------
+/// realWidth: 1080.0, realHeight: 2274.0, pixelRatio: 2.625, Padding(left: 0, top: 83, right: 0, bottom: 0)
+/// realWidth: 720, realHeight: 1280.0, pixelRatio: 2.0, Padding(left: 0, top: 48, right: 0, bottom: 0)
 class AppSizes {
   AppSizes._();
 
@@ -37,32 +37,7 @@ class AppSizes {
     return _instance;
   }
 
-  void _initial() {
-    _prepareSizes();
 
-    //----------------- onMetricsChanged -----------------
-    void onMetricsChanged(){
-      final oldW = realPixelWidth;
-      final oldH = realPixelHeight;
-      _prepareSizes();
-
-      /// Note: if below listener be comment, auto orientation reBuilding not work {OrientationBuilder()}
-      _systemMetricFunc?.call();
-
-      for(final f in onMetricListeners){
-        try{
-          f.call(oldW, oldH, realPixelWidth, realPixelHeight);
-        }
-        catch (e){/**/}
-      }
-    }
-
-    //----------------- onLocalChanged -----------------
-    //void onLocalChanged(){}
-
-    //ui.window.onLocaleChanged = onLocalChanged;
-    ui.window.onMetricsChanged = onMetricsChanged;
-  }
 
   void _prepareSizes() {
     realPixelWidth = ui.window.physicalSize.width;
@@ -87,22 +62,59 @@ class AppSizes {
     }
   }
 
-  void addMetricListener(OnResizeScreen lis){
+  void _initial() {
+    _prepareSizes();
+
+    //----------------- onMetricsChanged -----------------
+    void onMetricsChanged(){
+      final oldW = realPixelWidth;
+      final oldH = realPixelHeight;
+      _prepareSizes();
+
+      _systemMetricFunc?.call();
+      /*if(oldW == realHeight && oldH == realWidth) {
+        return;
+      }*/
+
+      for(final f in onMetricListeners){
+        try{
+          f.call(oldW, oldH, realPixelWidth, realPixelHeight);
+        }
+        catch (e){/**/}
+      }
+    }
+
+    //----------------- onLocalChanged -----------------
+    void onLocalChanged(){
+    }
+
+    ui.window.onLocaleChanged = onLocalChanged;
+    /// Note: if below listener be set, auto orientation reBuild not work {OrientationBuilder()}
+    ui.window.onMetricsChanged = onMetricsChanged;
+  }
+
+  /*static void detectSizeBy(BoxConstraints constraints, Orientation orientation) {
+    if (orientation == Orientation.landscape) {
+      appWidth = constraints.maxHeight;
+      appHeight = constraints.maxWidth;
+    }
+    else {
+      appWidth = constraints.maxWidth;
+      appHeight = constraints.maxHeight;
+    }
+    imageMultiplier = appWidth / 100;
+    textMultiplier = appHeight / 100;
+    heightMultiplier = appHeight / 100;
+  }*/
+
+  void addMetricListener(Function(double oldW, double oldH, double newW, double newH) lis){
     onMetricListeners.add(lis);
   }
 
-  void removeMetricListener(OnResizeScreen lis){
+  void removeMetricListener(Function lis){
     onMetricListeners.remove(lis);
   }
   ///●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●
-  static double? getPixelRatio(){
-    return instance.pixelRatio;
-  }
-
-  static double multiplierSize(double size){
-    return size * instance.heightMultiplier; // ~6.4
-  }
-
   static ui.FlutterWindow getWindow(){
     return ui.window;
   }
@@ -125,25 +137,41 @@ class AppSizes {
     return over / 2;
   }
 
-  static double webSize(double s){
+  double mTextSize(double tSize){
+    return tSize * textMultiplier;
+  }
+
+  double mImageSize(double iSize){
+    return iSize * imageMultiplier;
+  }
+
+  double mSize(double size){
+    return size * heightMultiplier; // ~6.4
+  }
+
+  double fwSize(double s){
     if(kIsWeb) {
       return s * 1.3;
     }
     return s;
   }
 
-  static double webTextFactor(double fact){
+  static double fwTextFactor(double fact){
     if(kIsWeb) {
       return fact * 1.4;
     }
     return fact;
   }
 
-  static double webFontSize(double size){
+  static double fwFontSize(double size){
     if(kIsWeb) {
       return size * 1.3;
     }
     return size;
+  }
+
+  double? getPixelRatio(){
+    return pixelRatio;
   }
 
   static double getPixelRatioBy(BuildContext context){
@@ -191,7 +219,7 @@ class AppSizes {
 
   static double getViewPortHeight(BuildContext context){
     final full = MediaQuery.of(context).size.height;
-    final status = MediaQuery.of(context).padding.top;
+    final status = MediaQuery.of(context).viewPadding.top; // this is variable
     const appBar = kToolbarHeight;
 
     return full - (status + appBar);
