965f2f6203c908aaa51dcc2c91136ffd78a48511
ali bagheri <bagheri_a@nicode.org>
2023-08-20 11:48:22 +0330

    1402-05-29, c3, work

--- lib/constants.dart
+++ lib/constants.dart
@@ -8,7 +8,7 @@ class Constants {
   static String appTitle = 'Big Bango';
   static const _major = 1;
   static const _minor = 0;
-  static const _patch = 0;
+  static const _patch = 4;
 
   static String appVersionName = '$_major.$_minor.$_patch';
   static int appVersionCode = _major *10000 + _minor *100 + _patch;
